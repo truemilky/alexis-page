f54abdea92491cd4c7e4fb5f92d4f30e49904462
truemilky <truemilky@gmail.com>
2021-03-05 19:08:33 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,59 +1,36 @@
-var person = document.querySelector(".our-team__member__01")
-var about = document.querySelector(".our-team__about")
+var persons = document.querySelector(".our-team__members");
+var defaultPerson = document.querySelector(".our-team__about-inner");
+var about = document.querySelector(".our-team__about");
 
-person.addEventListener ("click", function(e) {
-  about.classList.add("display-block");
+persons.addEventListener ("click", function(event) {
+  defaultPerson.classList.remove("active");
+  defaultPerson.classList.add("display-block");
+});
+
+var burger = document.querySelector(".header__burger");
+var burgerLine = document.querySelector(".header__burger-line");
+var headerNav = document.querySelector(".header__nav");
+var headerMenu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", function(event){
+  event.preventDefault();
+  burger.classList.toggle("active");
+  burgerLine.classList.toggle("active");
+  headerMenu.classList.toggle("active");
+  headerNav.classList.toggle("active");
 });
 
 $(document).ready(function() {
+  $("#slick").slick({
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+
   $('.testimonials__slick').click(function(event) {
     $(this).toggleClass('active').next().slideToggle(300);
   });
 }) ;
 
-var person = document.querySelector(".our-team__member__02")
-var about = document.querySelector(".our-team__about")
-
-person.addEventListener ("click", function(e) {
-  about.classList.add("display-block");
-});
-
-$(document).ready(function() {
-  $('.testimonials__slick').click(function(event) {
-    $(this).toggleClass('active').next().slideToggle(300);
-  });
-}) ;
-
-var person = document.querySelector(".our-team__member__03")
-var about = document.querySelector(".our-team__about")
-
-person.addEventListener ("click", function(e) {
-  about.classList.add("display-block");
-});
-
-$(document).ready(function() {
-  $('.testimonials__slick').click(function(event) {
-    $(this).toggleClass('active').next().slideToggle(300);
-  });
-}) ;
-
-var person = document.querySelector(".our-team__member__04")
-var about = document.querySelector(".our-team__about")
-
-person.addEventListener ("click", function(e) {
-  about.classList.add("display-block");
-});
-
-$(document).ready(function() {
-  $('.testimonials__slick').click(function(event) {
-    $(this).toggleClass('active').next().slideToggle(300);
-  });
-}) ;
-
-$("#slick").slick({
-  dots: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false
-});
